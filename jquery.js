ea6de23e9ad1d1f2cf72278123c41f5504fdb393
Patrick Mexal <patrickmexal@gmail.com>
2018-02-11 18:43:37 -0700

    update six

--- jquery.js
+++ jquery.js
@@ -3,12 +3,12 @@ $(function() {
 		axis: 'x',
 		containment: 'parent',
 		drag: function(event, ui) {
-			if (ui.position.left > 550) {
+			if (ui.position.left > 750) {
 				$("#well").fadeOut();
 			}
 		},
 		stop: function(event, ui) {
-			if (ui.position.left < 651) {
+			if (ui.position.left < 751) {
 				$(this).animate({
 					left: 0
 				})
